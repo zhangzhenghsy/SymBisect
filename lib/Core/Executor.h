2ed179a3292b055e8cf65064b8ecbc9e71239cde
zhangzhenghsy <zzhan173@zerg.cs.ucr.edu>
2023-06-01 23:56:31 -0700

    Forcely break from loop when a limit is reach, even if it's unconditional cond 2

--- lib/Core/Executor.h
+++ lib/Core/Executor.h
@@ -300,7 +300,7 @@ private:
   void executeInstruction(ExecutionState &state, KInstruction *ki);
 
   /// add by zheng: check whether the BB loop limit is reached, terminate the state if so
-  void checkLoopLimit(ExecutionState &state, KInstruction *ki, unsigned int looplimit, bool terminate);
+  bool checkLoopLimit(ExecutionState &state, KInstruction *ki, unsigned int looplimit, bool terminate);
   /// add by zheng: log the new added constraint when forking states at the branch 
   void logNewConstraint(ExecutionState &state, KInstruction *ki);
   /// add by zheng: check if there is a targetBB guidance for current Br branch Inst
